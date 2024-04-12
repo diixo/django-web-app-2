bec0694738c6451f2f72a7492cad316f8310f569
diixo <anatolii.podoba@gmail.com>
2024-04-13 00:23:52 +0300

    Implemented start routing for new pages: pictures.html, upload.html

--- core/app_main/views.py
+++ core/app_main/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def main(request):
     return render(request, "app_main/index.html", context={"title": "app_main"})
+
+def upload(request):
+    return render(request, "app_main/upload.html", context={"title": "upload image"})
+
+def pictures(request):
+    return render(request, "app_main/pictures.html", context={"title": "pictures output"})
