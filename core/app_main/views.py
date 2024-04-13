5cbc9ee4ef1b4b7c78e382ffb3a6feac7a073285
diixo <anatolii.podoba@gmail.com>
2024-04-13 08:54:33 +0300

    Implemented save-procedure, pictures procedure

--- core/app_main/views.py
+++ core/app_main/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.conf import settings
 from .forms import PictureForm
 from .models import Picture
 
@@ -7,8 +8,15 @@ def main(request):
     return render(request, "app_main/index.html", context={"title": "app_main"})
 
 def upload(request):
+    if request.method == "POST":
+        form = PictureForm(request.POST, request.FILES, instance=Picture())
+        if form.is_valid():
+            form.save()
+            return redirect(to="app_main:pictures")
+
     form = PictureForm(instance=Picture()) # bind Picture-model to PictureForm
     return render(request, "app_main/upload.html", context={"title": "upload image", "form": form})
 
 def pictures(request):
-    return render(request, "app_main/pictures.html", context={"title": "pictures output"})
+    imgs = Picture.objects.all()
+    return render(request, "app_main/pictures.html", context={"title": "pictures output", "pictures": imgs, "media":settings.MEDIA_URL})
